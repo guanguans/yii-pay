949945e12bc54ba1577c4c97fde8374dbcd37977
guanguans <yzmguanguan@gmail.com>
2020-03-18 11:45:00 +0800

    Add test file

---
+++ tests/PayTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the guanguans/yii-pay.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Tests;
+
+class PayTest extends TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function testTest()
+    {
+        $this->assertIsString('这是一个测试');
+    }
+}

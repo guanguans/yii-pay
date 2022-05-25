7dbfb0ccbfe90447c0df6f0fbee01c68c93cd230
yaozm <ityaozm@gmail.com>
2022-05-25 15:54:48 +0800

    Update CI config files

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+
+All notable changes to `guanguans/yii-pay` will be documented in this file
---
+++ _ide_helper.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the guanguans/yii-pay.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+class Yii
+{
+    /**
+     * @var Application
+     */
+    public static $app;
+}
+
+/**
+ * @property \Guanguans\YiiPay\Pay $pay
+ */
+class Application extends \yii\web\Application
+{
+}
